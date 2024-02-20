9736fdf353b1eab67cb302b985aeed4f4d076cad
robsonbittencourt <robson.luizv@gmail.com>
2024-02-20 17:44:53 -0300

    Refactory Target Pokémon strucutre

--- src/app/pokemon-main/pokemon-main.component.ts
+++ src/app/pokemon-main/pokemon-main.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { DoCheck, KeyValueDiffers, KeyValueDiffer } from '@angular/core';
+import { KeyValueDiffers, KeyValueDiffer } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
 import axios from 'axios';
 import { startWith, map } from 'rxjs/operators';
-import { calculate, Generations, Field, Pokemon, Move, MOVES, ITEMS, NATURES, TYPE_CHART, SPECIES, ABILITIES } from '@smogon/calc';
+import { calculate, Generations, Field, Move, MOVES, ITEMS, NATURES, TYPE_CHART, SPECIES, ABILITIES } from '@smogon/calc';
 import { Koffing } from 'koffing'
-import { TargetPokemon } from './target-pokemon';
+import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-pokemon-main',
@@ -36,27 +36,26 @@ export class PokemonMainComponent implements OnInit {
   filteredTeraTypes: Observable<string[]>;
   filteredMoves: Observable<string[]>;
 
-  tabletsOfRuinActive = false;
-  vesselOfRuinActive = false;
-  swordOfRuinActive = false;
-  beadsOfRuinActive = false;
+  field = {
+    tabletsOfRuinActive: false,
+    vesselOfRuinActive: false,
+    swordOfRuinActive: false,
+    beadsOfRuinActive: false
+  }
 
   EV_ZERO = 0
   FIRST_EV = 4
   MAX_EVS = 508
 
-  differ: KeyValueDiffer<string, any>;
-  constructor(private differs: KeyValueDiffers) {
-    this.differ = this.differs.find({}).create();
-  }
+  private differ: KeyValueDiffer<string, any>;
+  private differField: KeyValueDiffer<string, any>;
+  public pokemon: Pokemon
+  public pokePaste = ""
 
-  ngDoCheck() {
-    const change = this.differ.diff(this);
-    if (change) {
-      change.forEachChangedItem(item => {
-        this.calcDamageToAll()
-      });
-    }
+  constructor(private differs: KeyValueDiffers) {
+    this.pokemon = new Pokemon("Flutter Mane", "Timid", "Choice Specs", "Protosynthesis", "Fairy", true, { hp: 0, atk: 0, def: 0, spa: 252, spd: 0, spe: 252 }, "Moon Blast")
+    this.differ = this.differs.find(this.pokemon).create();
+    this.differField = this.differs.find(this.field).create();
   }
 
   ngOnInit() {
@@ -93,6 +92,13 @@ export class PokemonMainComponent implements OnInit {
     this.calcDamageToAll();
   }
 
+  ngDoCheck() {
+    const change = this.differ.diff(this.pokemon) || this.differField.diff(this.field);
+    if (change) {
+        this.calcDamageToAll()
+    }
+  }
+
   private _filter(value: string, values: string[]): string[] {
     const filterValue = this._normalizeValue(value);
     return values.filter(name => this._normalizeValue(name).includes(filterValue));
@@ -103,154 +109,67 @@ export class PokemonMainComponent implements OnInit {
   }
 
   gen = Generations.get(9);
-  field = new Field({ gameType: 'Doubles' });
 
-  ragingBolt = new Pokemon(this.gen, 'Raging Bolt', {
-    item: 'Booster Energy',
-    nature: 'Modest',
-    evs: { hp: 244, spa: 252, spd: 12 },
-    level: 50,
-    teraType: 'Fairy'
-  })
-
-  walkingWake = new Pokemon(this.gen, 'Walking Wake', {
-    item: 'Life Orb',
-    nature: 'Timid',
-    evs: { hp: 4, spa: 252, spe: 252 },
-    level: 50,
-    teraType: 'Poison'
-  })
-
-  gougingFire = new Pokemon(this.gen, 'Gouging Fire', {
-    item: 'Clear Amulet',
-    nature: 'Adamant',
-    evs: { hp: 4, atk: 252, spe: 252 },
-    level: 50,
-    teraType: 'Water'
-  })
-
-  entei = new Pokemon(this.gen, 'Entei', {
-    item: 'Sitrus Berry',
-    nature: 'Adamant',
-    evs: { atk: 252, def: 4, spd: 252 },
-    level: 50,
-    teraType: 'Grass'
-  })
-
-  incineroar = new Pokemon(this.gen, 'Incineroar', {
-    item: 'Assault Vest',
-    nature: 'Careful',
-    evs: { hp: 252, atk: 4, spd: 252 },
-    level: 50,
-    teraType: 'Water'
-  })
-
-  urshifu = new Pokemon(this.gen, 'Urshifu', {
-    item: 'Choice Scarf',
-    nature: 'Jolly',
-    evs: { atk: 252, spd: 4, spe: 252 },
-    level: 50,
-    teraType: 'Water'
-  })
-
-  landorus = new Pokemon(this.gen, 'Landorus', {
-    item: 'Life Orb',
-    nature: 'Timid',
-    evs: { spa: 252, spd: 4, spe: 252 },
-    level: 50,
-    teraType: 'Flying'
-  })
-
-  ogerponWellspring = new Pokemon(this.gen, 'Ogerpon-Wellspring', {
-    item: 'Wellspring Mask',
-    nature: 'Adamant',
-    evs: { hp: 252, atk: 76, def: 148, spd: 28, spe: 4 },
-    level: 50,
-    teraType: 'Water'
-  })
-
-  targets: TargetPokemon[] = [
-    new TargetPokemon(this.ragingBolt, true),
-    new TargetPokemon(this.walkingWake, true),
-    new TargetPokemon(this.gougingFire),
-    new TargetPokemon(this.entei),
-    new TargetPokemon(this.incineroar),
-    new TargetPokemon(this.urshifu, true),
-    new TargetPokemon(this.landorus),
-    new TargetPokemon(this.ogerponWellspring)
+  ragingBolt = new Pokemon('Raging Bolt', "Modest", "Booster Energy", "Protosynthesis", "Fairy", true, { hp: 244, spa: 252, spd: 12 })
+  walkingWake = new Pokemon('Walking Wake', "Timid", "Life Orb", "Protosynthesis", "Poison", true, { hp: 4, spa: 252, spe: 252 })
+  gougingFire = new Pokemon('Gouging Fire', "Adamant", "Clear Amulet", "Protosynthesis", "Water", false, { hp: 4, atk: 252, spe: 252 })
+  entei = new Pokemon('Entei', "Adamant", "Sitrus Berry", "Inner Focus", "Grass", false, { atk: 252, def: 4, spd: 252 })
+  incineroar = new Pokemon('Incineroar', "Careful", "Assault Vest", "Intimidate", "Water", false, { hp: 252, atk: 4, spd: 252 })
+  urshifu = new Pokemon('Urshifu', "Jolly", "Choice Scarf", "Unseen Fist", "Water", true, { atk: 252, spd: 4, spe: 252 })
+  landorus = new Pokemon('Landorus', "Timid", "Life Orb", "Sheer Force", "Flying", false, { spa: 252, spd: 4, spe: 252 })
+  ogerponWellspring = new Pokemon('Ogerpon-Wellspring', "Adamant", "Wellspring Mask", "Water Absorb", "Water", false, { hp: 252, atk: 76, def: 148, spd: 28, spe: 4 })
+  
+  targets: Pokemon[] = [
+    this.ragingBolt,
+    this.walkingWake,
+    this.gougingFire,
+    this.entei,
+    this.incineroar,
+    this.urshifu,
+    this.landorus,
+    this.ogerponWellspring
   ]
 
-  public pokemonName = "Flutter Mane"
-  public nature = "Timid"
-  public item = "Choice Specs"
-  public ability: any = "Protosynthesis"
-  public teraType = "Fairy"
-  public moveName = 'Moon Blast'
-  public teraTypeActive = true
-  public pokePaste = ""
-
-  public current = new TargetPokemon(new Pokemon(this.gen, this.pokemonName, {
-    nature: this.nature,
-    item: this.item,
-    ability: this.ability,
-    teraType: this.teraTypeActive ? this.teraType as any : null,
-    evs: { hp: 0, atk: 0, def: 0, spa: 252, spd: 0, spe: 252 },
-    level: 50
-  }), this.teraTypeActive)
-
-  public previous = new TargetPokemon(this.current.pokemon.clone())
-
   onPokemonSelected(selectedPokemon: string) {
-    this.pokemonName = selectedPokemon
+    this.pokemon.name = selectedPokemon
   }
 
   onNatureSelected(selectedNature: string) {
-    this.nature = selectedNature
+    this.pokemon.nature = selectedNature
   }
 
   onTeraTypeSelected(selectedTeraType: string) {
-    this.teraType = selectedTeraType
+    this.pokemon.teraType = selectedTeraType
   }
 
   onItemSelected(selectedItem: string) {
-    this.item = selectedItem
+    this.pokemon.item = selectedItem
   }
 
   onAbilitySelected(selectedAbility: string) {
-    this.ability = selectedAbility
+    this.pokemon.ability = selectedAbility
   }
 
   onMoveSelected(selectedMove: string) {
-    this.moveName = selectedMove
+    this.pokemon.move = selectedMove
   }
 
   onChangeEvValue() {
-    if (this.current.totalEvs() <= this.MAX_EVS) {
-      this.previous = new TargetPokemon(this.current.pokemon.clone())
-      this.current = new TargetPokemon(this.current.pokemon.clone())
+    if (this.pokemon.totalEvs() <= this.MAX_EVS) {
+      this.pokemon.evs = this.pokemon.evs
     } else {
-      this.current = new TargetPokemon(this.previous.pokemon.clone())
+      this.pokemon.evs = this.pokemon.evsStorage
     }    
   }
 
   beforeChangeEvValue() {
-    if (this.current.totalEvs() <= this.MAX_EVS) {
-      this.previous = new TargetPokemon(this.current.pokemon.clone())
-      this.current = new TargetPokemon(this.current.pokemon.clone())
+    if (this.pokemon.totalEvs() <= this.MAX_EVS) {
+      this.pokemon.evs = this.pokemon.evs
     }
   }
 
   addPokemon() {
-    this.targets.push(
-      new TargetPokemon(new Pokemon(this.gen, this.pokemonName, {
-        nature: this.nature,
-        item: this.item,
-        ability: this.ability,
-        teraType: this.teraTypeActive ? this.teraType as any : null,
-        evs: this.current.pokemon.evs,
-        level: 50
-      }), this.teraTypeActive)
-    )
+    this.targets.push(this.pokemon.clone())
     this.calcDamageToAll()
   }
 
@@ -259,15 +178,9 @@ export class PokemonMainComponent implements OnInit {
       .then(res => {
         const parsedTeam = Koffing.parse(res.data)
         JSON.parse(parsedTeam.toJson()).teams[0].pokemon.forEach((poke: any) => {
+          const evs = { hp: poke.evs.hp, atk: poke.evs.atk, def: poke.evs.def, spa: poke.evs.spa, spd: poke.evs.spd, spe: poke.evs.spe }
           this.targets.push(
-            new TargetPokemon(new Pokemon(this.gen, poke.name, {
-              nature: poke.nature,
-              item: poke.item,
-              ability: poke.ability,
-              evs: { hp: poke.evs.hp, atk: poke.evs.atk, def: poke.evs.def, spa: poke.evs.spa, spd: poke.evs.spd, spe: poke.evs.spe },
-              teraType: poke.teraType,
-              level: 50
-            }))
+            new Pokemon(poke.name, poke.nature, poke.item, poke.ability, poke.teraType, false, evs)
           )
         })
         this.calcDamageToAll()
@@ -288,35 +201,36 @@ export class PokemonMainComponent implements OnInit {
   }
 
   terastalyzePokemon(index: number) {
-    this.targets[index].changeTeraStatus()
+    const teraActived = this.targets[index].teraTypeActive()
+    this.targets[index].changeTeraStatus(!teraActived)
     this.calcDamageToAll()
   }
 
-  calculateMin(evValue: number) {
+  calculateMin(evValue: number | undefined) {
     if (evValue == this.EV_ZERO || evValue == this.FIRST_EV) return 0
     
     return 4
   }
 
-  calculateEvStep(evValue: number) {
+  calculateEvStep(evValue: number | undefined) {
     if (evValue == this.EV_ZERO) return 4
     if (evValue == this.FIRST_EV) return 6
     
     return 8
   }
 
-  calcDamage(attackerPoke: Pokemon, target: TargetPokemon, move: Move) {
+  calcDamage(attackerPoke: Pokemon, target: Pokemon, move: Move) {
     const gen = Generations.get(9);
     const result = calculate(
       gen,
-      attackerPoke,
-      target.pokemon,
+      attackerPoke.pokemonSmogon,
+      target.pokemonSmogon,
       move,
-      this.field
+      new Field({ gameType: 'Doubles' })
     );
 
     //TODO refactory
-    if ((result.move.category == 'Special' && this.vesselOfRuinActive && !this.beadsOfRuinActive) || (result.move.category == 'Physical' && this.tabletsOfRuinActive && !this.swordOfRuinActive)) {
+    if ((result.move.category == 'Special' && this.field.vesselOfRuinActive && !this.field.beadsOfRuinActive) || (result.move.category == 'Physical' && this.field.tabletsOfRuinActive && !this.field.swordOfRuinActive)) {
       (result.damage as Array<number>)[0] = Math.ceil((result.damage as Array<number>)[0] * 0.75);
       (result.damage as Array<number>)[1] = Math.ceil((result.damage as Array<number>)[1] * 0.75);
       (result.damage as Array<number>)[2] = Math.ceil((result.damage as Array<number>)[2] * 0.75);
@@ -335,7 +249,7 @@ export class PokemonMainComponent implements OnInit {
       (result.damage as Array<number>)[15] = Math.ceil((result.damage as Array<number>)[15] * 0.75);
     }
 
-    if ((result.move.category == 'Special' && this.beadsOfRuinActive && !this.vesselOfRuinActive) || (result.move.category == 'Physical' && this.swordOfRuinActive && !this.tabletsOfRuinActive)) {
+    if ((result.move.category == 'Special' && this.field.beadsOfRuinActive && !this.field.vesselOfRuinActive) || (result.move.category == 'Physical' && this.field.swordOfRuinActive && !this.field.tabletsOfRuinActive)) {
       (result.damage as Array<number>)[0] = Math.ceil((result.damage as Array<number>)[0] * 1.33);
       (result.damage as Array<number>)[1] = Math.ceil((result.damage as Array<number>)[1] * 1.33);
       (result.damage as Array<number>)[2] = Math.ceil((result.damage as Array<number>)[2] * 1.33);
@@ -372,19 +286,10 @@ export class PokemonMainComponent implements OnInit {
   }
 
   calcDamageToAll() {
-    const move = new Move(this.gen, this.moveName)
-
-    const pokemon = new Pokemon(this.gen, this.pokemonName, {
-      item: this.item,
-      nature: this.nature,
-      ability: this.ability,
-      evs: this.current.pokemon.evs,
-      teraType: this.teraTypeActive ? this.teraType as any : null,
-      level: 50
-    })
+    const move = new Move(this.gen, this.pokemon.move)
 
     this.targets.forEach((target) => {
-      this.calcDamage(pokemon, target, move)
+      this.calcDamage(this.pokemon, target, move)
     })
 
     this.targets
---
+++ src/app/pokemon-main/pokemon.ts
@@ -0,0 +1,174 @@
+import { Pokemon as PokemonSmogon, Generations } from "@smogon/calc";
+import { StatsTable, TypeName } from "@smogon/calc/dist/data/interface";
+
+export class Pokemon {
+  public pokemonSmogon: PokemonSmogon
+  public teraTypeStorage: string
+  public evsStorage: Partial<StatsTable> & { spc?: number; }
+  private moveStorage: string
+
+  public damage: number = 0
+  public result: String = ""
+  public koChance: String = ""
+
+  constructor(name: string, nature: string, item: string, ability: string, teraType: string, teraTypeActive: boolean = false, evs: Partial<StatsTable> & { spc?: number; }, move: string = "") {
+    this.pokemonSmogon = new PokemonSmogon(Generations.get(9), name, {
+      nature: nature,
+      item: item,
+      ability: ability,
+      teraType: teraTypeActive ? teraType as TypeName : undefined,
+      evs: evs,
+      level: 50
+    })
+
+    this.teraTypeStorage = teraType
+    this.evsStorage = evs
+    this.moveStorage = move
+  }
+
+  public get name(): string {
+    return this.pokemonSmogon.name
+  }
+
+  public set name(name: string) {
+    this.pokemonSmogon = this.buildPokemonSmogon({ name: name })
+  }
+
+  public get lowerCaseName(): string {
+    return this.pokemonSmogon.name.toLocaleLowerCase().replace(" ", "-")
+  }
+
+  public get nature(): string {
+    return this.pokemonSmogon.nature as string
+  }
+
+  public set nature(nature: string) {
+    this.pokemonSmogon = this.buildPokemonSmogon({ nature: nature })
+  }
+
+  public get item(): string {
+    return this.pokemonSmogon.item as string
+  }
+
+  public set item(item: string) {
+    this.pokemonSmogon = this.buildPokemonSmogon({ item: item })
+  }
+
+  public get ability(): string {
+    return this.pokemonSmogon.ability as string
+  }
+
+  public set ability(ability: string) {
+    this.pokemonSmogon = this.buildPokemonSmogon({ ability: ability })
+  }
+
+  public get evs(): Partial<StatsTable> & { spc?: number; } {
+    return this.pokemonSmogon.evs
+  }
+
+  public set evs(evs: Partial<StatsTable> & { spc?: number; }) {
+    this.evsStorage = evs
+    this.pokemonSmogon = this.buildPokemonSmogon({ evs: evs })      
+  }
+
+  public get teraType(): string {
+    return this.pokemonSmogon.teraType as string
+  }
+
+  public set teraType(teraType: string) {
+    this.teraTypeStorage = teraType
+    this.pokemonSmogon = this.buildPokemonSmogon({ teraType: teraType, teraTypeActive: this.teraTypeActive() })
+  }
+
+  public teraTypeActive(): boolean {
+    return this.pokemonSmogon.teraType != undefined
+  }
+
+  public changeTeraStatus(teraTypeActive: boolean) {
+    if (teraTypeActive) {
+      this.pokemonSmogon = this.buildPokemonSmogon({ teraType: this.teraTypeStorage, teraTypeActive: true })
+    } else {
+      this.pokemonSmogon = this.buildPokemonSmogon({ teraType: undefined, teraTypeActive: false })
+    }
+  }
+
+  public get move(): string {
+    return this.moveStorage
+  }
+
+  public set move(move: string) {
+    this.moveStorage = move
+  }
+
+  public get hp(): number {
+    return this.pokemonSmogon.stats.hp
+  }
+
+  public get atk(): number {
+    return this.pokemonSmogon.stats.atk
+  }
+
+  public get def(): number {
+    return this.pokemonSmogon.stats.def
+  }
+
+  public get spa(): number {
+    return this.pokemonSmogon.stats.spa
+  }
+
+  public get spd(): number {
+    return this.pokemonSmogon.stats.spd
+  }
+
+  public get spe(): number {
+    return this.pokemonSmogon.stats.spe
+  }
+
+  public clone(): Pokemon {
+    return new Pokemon(this.name, this.nature, this.item, this.ability, this.teraType, this.teraTypeActive(), this.evs, this.move)
+  }
+
+  public totalEvs(): number {
+    return this.pokemonSmogon.evs.hp + this.pokemonSmogon.evs.atk + this.pokemonSmogon.evs.def + this.pokemonSmogon.evs.spa + this.pokemonSmogon.evs.spd + this.pokemonSmogon.evs.spe
+  }
+
+  public evsDescription(): string {
+    var evsDescription = ""
+
+    if (this.evs.hp && this.evs.hp != 0) evsDescription += `hp: ${this.evs.hp} `
+    if (this.evs.atk && this.evs.atk != 0) evsDescription += `atk: ${this.evs.atk} `
+    if (this.evs.def && this.evs.def != 0) evsDescription += `def: ${this.evs.def} `
+    if (this.evs.spa && this.evs.spa != 0) evsDescription += `spa: ${this.evs.spa} `
+    if (this.evs.spd && this.evs.spd != 0) evsDescription += `spd: ${this.evs.spd} `
+    if (this.evs.spe && this.evs.spe != 0) evsDescription += `spe: ${this.evs.spe}`
+
+    return evsDescription
+  }
+
+  private buildPokemonSmogon({ name, nature, item, ability, teraType, teraTypeActive, evs }: { name?: string; nature?: string; item?: string; ability?: string; teraType?: string; teraTypeActive?: boolean; evs?: Partial<StatsTable> & { spc?: number; }} = {}): PokemonSmogon {
+    return new PokemonSmogon(Generations.get(9), name ? name : this.pokemonSmogon.name, {
+      nature: nature ? nature : this.pokemonSmogon.nature,
+      item: item ? item : this.pokemonSmogon.item,
+      ability: ability ? ability : this.pokemonSmogon.ability,
+      teraType: this.buildTeraType(teraType, teraTypeActive) as TypeName,
+      evs: evs ? evs : this.pokemonSmogon.evs,
+      level: 50
+    })
+  }
+
+  private buildTeraType(teraType?: string, teraTypeActive?: boolean) {
+    if (teraTypeActive == false) {
+      return undefined
+    }
+
+    if (teraTypeActive == undefined && !this.teraTypeActive()) {
+      return undefined
+    }
+
+    if (teraTypeActive == undefined && this.teraTypeActive()) {
+      return this.pokemonSmogon.teraType
+    }
+
+    return teraType
+  }
+}
