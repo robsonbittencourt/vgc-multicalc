1333a5947c8957981c1f44c60d209a334ab68785
robsonbittencourt <robson.luizv@gmail.com>
2024-10-07 22:45:14 -0300

    Add first version of damage in one-vs-one

--- src/app/simple-calc/simple-calc.component.ts
+++ src/app/simple-calc/simple-calc.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { DamageCalculatorService } from 'src/lib/damage-calculator.service';
+import { DamageResult } from 'src/lib/damage-result';
 import { MoveSet } from 'src/lib/moveset';
 import { Pokemon } from 'src/lib/pokemon';
 import { DataStore } from '../data-store.service';
@@ -13,10 +15,28 @@ export class SimpleCalcComponent {
   leftPokemon: Pokemon = new Pokemon("Miraidon", { nature: "Timid", item: "Choice Specs", teraType: "Electric", evs: { hp: 4, spa: 252, spe: 252 }, moveSet: new MoveSet("Electro Drift", "Parabolic Charge", "Volt Switch", "Draco Meteor") })
   rightPokemon: Pokemon = new Pokemon('Koraidon', { nature: "Adamant", item: "Clear Amulet", teraType: "Fire", evs: { hp: 36, atk: 220, spe: 252 }, moveSet: new MoveSet("Flame Charge", "Collision Course", "Flare Blitz", "Protect") })
 
-  constructor(public data: DataStore) { }
+  leftDamageResult: DamageResult
+  rightDamageResult: DamageResult
+
+  @Output() 
+  dataChangedEvent = new EventEmitter<any>()
+
+  constructor(public data: DataStore, private damageCalculator: DamageCalculatorService) {}
+
+  leftPokemonChanged() {
+    this.leftDamageResult = this.damageCalculator.calcDamage(this.leftPokemon, this.rightPokemon, this.data.field, this.data.extraFieldOptions.criticalHit)
+    this.dataChangedEvent.emit()
+  }
+
+  rightPokemonChanged() {
+    this.rightDamageResult = this.damageCalculator.calcDamage(this.rightPokemon, this.leftPokemon, this.data.field, this.data.extraFieldOptions.criticalHit)
+    this.dataChangedEvent.emit()
+  }
 
   fieldChanged() {
-
+    this.leftDamageResult = this.damageCalculator.calcDamage(this.leftPokemon, this.rightPokemon, this.data.field, this.data.extraFieldOptions.criticalHit)
+    this.rightDamageResult = this.damageCalculator.calcDamage(this.rightPokemon, this.leftPokemon, this.data.field, this.data.extraFieldOptions.criticalHit)
+    this.dataChangedEvent.emit()
   }
 
 }
