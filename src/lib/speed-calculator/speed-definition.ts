45b2176f8c9384f57c8175f780c100dff6d1d0ba
robsonbittencourt <robson.luizv@gmail.com>
2024-10-30 11:19:26 -0300

    Fix track by warning

--- src/lib/speed-calculator/speed-definition.ts
+++ src/lib/speed-calculator/speed-definition.ts
@@ -1,9 +1,13 @@
+import { v4 as uuidv4 } from 'uuid';
+
 export class SpeedDefinition {
+  id: string
   pokemonName: string
   value: number
   description: string
 
   constructor(pokemonName: string, speed: number, description: string) {
+    this.id = uuidv4()
     this.pokemonName = pokemonName
     this.value = speed
     this.description = description
