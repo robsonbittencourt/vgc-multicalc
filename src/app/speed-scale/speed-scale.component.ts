631436345efc68179f1e420c3fe7e4b5cfb65eff
robsonbittencourt <robson.luizv@gmail.com>
2024-11-24 19:28:07 -0300

    Remove unused code

--- src/app/speed-scale/speed-scale.component.ts
+++ src/app/speed-scale/speed-scale.component.ts
@@ -31,29 +31,6 @@ export class SpeedScaleComponent {
   private differStatusModifiers: KeyValueDiffer<string, any>
   private differOptions: KeyValueDiffer<string, any>
 
-  statsModifierValue: number = 0
-  statsModifiers = [
-    { value: 6, viewValue: "+6"}, { value: 5, viewValue: "+5"}, { value: 4, viewValue: "+4"},
-    { value: 3, viewValue: "+3"}, { value: 2, viewValue: "+2"}, { value: 1, viewValue: "+1"},
-    { value: 0, viewValue: "--"},
-    { value: -1, viewValue: "-1"}, { value: -2, viewValue: "-2"}, { value: -3, viewValue: "-3"},
-    { value: -4, viewValue: "-4"}, { value: -5, viewValue: "-5"}, { value: -6, viewValue: "-6"},
-  ]
-  speedDropMoveActive: boolean = false
-
-  get statsModifier(): number {
-    return this.statsModifierValue
-  }
-
-  set statsModifier(statsModifier: number) {
-    this.statsModifierValue = statsModifier
-    this.options().speedModifier = statsModifier
-
-    if(statsModifier != 1) {
-      this.speedDropMoveActive = false
-    }
-  }
-  
   inSpeedRange: SpeedDefinition[]
 
   previousSpeedDefinition: SpeedDefinition[] = []
