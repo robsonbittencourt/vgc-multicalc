5793f485f55aa3056016d84bfb0a0504c5de7325
robsonbittencourt <robson.luizv@gmail.com>
2024-12-15 13:49:51 -0300

    Create more tests to smogon functions

--- src/lib/smogon-functions/smogon-functions.spec.ts
+++ src/lib/smogon-functions/smogon-functions.spec.ts
@@ -67,7 +67,35 @@ describe('SmogonFunctions', () => {
     expect(finalSpeed).toEqual(54)
   })
 
+  it('should return the speed of Iron Bundle with Quark Drive activated', () => {
+    const pokemon = new Pokemon("Iron Bundle", { abilityOn: true, nature: "Timid", evs: { spe: 252 } })
+    const field = new Field()
+
+    const finalSpeed = service.getFinalSpeed(pokemon, field)
+
+    expect(finalSpeed).toEqual(309)
+  })
+
+  it('should return the speed of Iron Bundle with Quark Drive activated by the Electric terrain', () => {
+    const pokemon = new Pokemon("Iron Bundle", { nature: "Timid", evs: { spe: 252 } })
+    const field = new Field()
+    field.terrain = "Electric"
+
+    const finalSpeed = service.getFinalSpeed(pokemon, field)
+
+    expect(finalSpeed).toEqual(309)
+  })
+
   it('should return the speed of Flutter Mane with Protosynthesis activated', () => {
+    const pokemon = new Pokemon("Flutter Mane", { abilityOn: true, item: "Choice Scarf", nature: "Timid", evs: { spe: 124 } })
+    const field = new Field()
+
+    const finalSpeed = service.getFinalSpeed(pokemon, field)
+
+    expect(finalSpeed).toEqual(423)
+  })
+
+  it('should return the speed of Flutter Mane with Protosynthesis activated by the Sun', () => {
     const pokemon = new Pokemon("Flutter Mane", { item: "Choice Scarf", nature: "Timid", evs: { spe: 124 } })
     const field = new Field()
     field.weather = "Sun"
@@ -88,6 +116,164 @@ describe('SmogonFunctions', () => {
     const finalSpeed = service.getFinalSpeed(pokemon, field, side)
 
     expect(finalSpeed).toEqual(846)
+  })
+
+  it('should return the speed of Sneasler with Unburden activated', () => {
+    const pokemon = new Pokemon("Sneasler", { ability: "Unburden", abilityOn: true, nature: "Jolly", evs: { spe: 252 } })
+    const field = new Field()
+
+    const finalSpeed = service.getFinalSpeed(pokemon, field)
+
+    expect(finalSpeed).toEqual(378)
+  })
+
+  it('should return the speed of Jumpluff with Chlorophyll in the Sun', () => {
+    const pokemon = new Pokemon("Jumpluff", { ability: "Chlorophyll", nature: "Timid", evs: { spe: 252 } })
+    const field = new Field()
+    field.weather = "Sun"
+
+    const finalSpeed = service.getFinalSpeed(pokemon, field)
+
+    expect(finalSpeed).toEqual(356)
+  })
+
+  it('should return the speed of Excadrill with Sand Rush in the Sandstorm', () => {
+    const pokemon = new Pokemon("Excadrill", { ability: "Sand Rush", nature: "Jolly", evs: { spe: 252 } })
+    const field = new Field()
+    field.weather = "Sand"
+
+    const finalSpeed = service.getFinalSpeed(pokemon, field)
+
+    expect(finalSpeed).toEqual(308)
+  })
+
+  it('should return the speed of Basculegion with Swift Swim in the Rain', () => {
+    const pokemon = new Pokemon("Basculegion", { ability: "Swift Swim", nature: "Jolly", evs: { spe: 252 } })
+    const field = new Field()
+    field.weather = "Rain"
+
+    const finalSpeed = service.getFinalSpeed(pokemon, field)
+
+    expect(finalSpeed).toEqual(286)
+  })
+
+  it('should return the speed of Beartic with Slush Rush in the Snow', () => {
+    const pokemon = new Pokemon("Beartic", { ability: "Slush Rush", nature: "Jolly", evs: { spe: 252 } })
+    const field = new Field()
+    field.weather = "Snow"
+
+    const finalSpeed = service.getFinalSpeed(pokemon, field)
+
+    expect(finalSpeed).toEqual(224)
+  })
+
+  it('should return the speed of Raichu-Alola with Surge Surfer in the Eletric Terrain', () => {
+    const pokemon = new Pokemon("Raichu-Alola", { ability: "Surge Surfer", nature: "Timid", evs: { spe: 252 } })
+    const field = new Field()
+    field.terrain = "Electric"
+
+    const finalSpeed = service.getFinalSpeed(pokemon, field)
+
+    expect(finalSpeed).toEqual(356)
+  })
+
+  it('should return the speed of Jolteon with Quick Feet when it has status condition', () => {
+    const pokemon = new Pokemon("Jolteon", { ability: "Quick Feet", status: "Burn", nature: "Timid", evs: { spe: 252 } })
+    const field = new Field()
+
+    const finalSpeed = service.getFinalSpeed(pokemon, field)
+
+    expect(finalSpeed).toEqual(300)
+  })
+
+  it('should return the speed of Regigigas with Slow Start when tha ability is on', () => {
+    const pokemon = new Pokemon("Regigigas", { ability: "Slow Start", abilityOn: true, nature: "Jolly", evs: { spe: 252 } })
+    const field = new Field()
+
+    const finalSpeed = service.getFinalSpeed(pokemon, field)
+
+    expect(finalSpeed).toEqual(83)
+  })
+
+  it('should return the speed of Regigigas with Slow Start when tha ability is off', () => {
+    const pokemon = new Pokemon("Regigigas", { ability: "Slow Start", abilityOn: false, nature: "Jolly", evs: { spe: 252 } })
+    const field = new Field()
+
+    const finalSpeed = service.getFinalSpeed(pokemon, field)
+
+    expect(finalSpeed).toEqual(167)
+  })
+
+  it('should return the Pokémon speed when hold Iron Ball', () => {
+    const pokemon = new Pokemon('Raging Bolt', { item: "Iron Ball", evs: { spe: 100 } })
+    
+    const finalSpeed = service.getFinalSpeed(pokemon)
+
+    expect(finalSpeed).toEqual(54)
+  })
+
+  it('should return the Pokémon speed when hold Macho Brace', () => {
+    const pokemon = new Pokemon('Raging Bolt', { item: "Macho Brace", evs: { spe: 100 } })
+    
+    const finalSpeed = service.getFinalSpeed(pokemon)
+
+    expect(finalSpeed).toEqual(54)
+  })
+
+  it('should return the Pokémon speed when hold Power Anklet', () => {
+    const pokemon = new Pokemon('Raging Bolt', { item: "Power Anklet", evs: { spe: 100 } })
+    
+    const finalSpeed = service.getFinalSpeed(pokemon)
+
+    expect(finalSpeed).toEqual(54)
+  })
+
+  it('should return the Pokémon speed when hold Power Band', () => {
+    const pokemon = new Pokemon('Raging Bolt', { item: "Power Band", evs: { spe: 100 } })
+    
+    const finalSpeed = service.getFinalSpeed(pokemon)
+
+    expect(finalSpeed).toEqual(54)
+  })
+
+  it('should return the Pokémon speed when hold Power Belt', () => {
+    const pokemon = new Pokemon('Raging Bolt', { item: "Power Belt", evs: { spe: 100 } })
+    
+    const finalSpeed = service.getFinalSpeed(pokemon)
+
+    expect(finalSpeed).toEqual(54)
+  })
+
+  it('should return the Pokémon speed when hold Power Bracer', () => {
+    const pokemon = new Pokemon('Raging Bolt', { item: "Power Bracer", evs: { spe: 100 } })
+    
+    const finalSpeed = service.getFinalSpeed(pokemon)
+
+    expect(finalSpeed).toEqual(54)
+  })
+
+  it('should return the Pokémon speed when hold Power Lens', () => {
+    const pokemon = new Pokemon('Raging Bolt', { item: "Power Lens", evs: { spe: 100 } })
+    
+    const finalSpeed = service.getFinalSpeed(pokemon)
+
+    expect(finalSpeed).toEqual(54)
+  })
+
+  it('should return the Pokémon speed when hold Power Weight', () => {
+    const pokemon = new Pokemon('Raging Bolt', { item: "Power Weight", evs: { spe: 100 } })
+    
+    const finalSpeed = service.getFinalSpeed(pokemon)
+
+    expect(finalSpeed).toEqual(54)
+  })
+
+  it('should return the Ditto speed when hold Quick Powder', () => {
+    const pokemon = new Pokemon('Ditto', { item: "Quick Powder", nature: "Jolly", evs: { spe: 252 } })
+    
+    const finalSpeed = service.getFinalSpeed(pokemon)
+
+    expect(finalSpeed).toEqual(220)
   })
 
 })
