e5779f068dcffa32d132fb14c1e3bab6680698e4
robsonbittencourt <robson.luizv@gmail.com>
2024-12-29 00:44:21 -0300

    Refactory hp badge component to use signals

--- src/app/features/simple-calc/pokemon-hp-badge/pokemon-hp-badge.component.ts
+++ src/app/features/simple-calc/pokemon-hp-badge/pokemon-hp-badge.component.ts
@@ -1,5 +1,5 @@
 import { NgStyle } from "@angular/common"
-import { AfterViewInit, Component, ElementRef, Input, ViewChild } from "@angular/core"
+import { Component, computed, ElementRef, input, viewChild } from "@angular/core"
 import { Items } from "@data/items"
 
 @Component({
@@ -8,172 +8,107 @@ import { Items } from "@data/items"
   styleUrls: ["./pokemon-hp-badge.component.scss"],
   imports: [NgStyle]
 })
-export class PokemonHpBadgeComponent implements AfterViewInit {
-  remainingHp: number
-  hpPercentage: number
-  hpBarColor: string
+export class PokemonHpBadgeComponent {
+  _actualSpriteName: string
+
+  name = input.required<string>()
+  hpBase = input.required<number>()
+  actualHp = input.required<number>()
+  spriteName = input.required<string>()
+  damageTaken = input.required<number>()
+  item = input.required({ transform: (value: string) => value.toLowerCase().replaceAll(" ", "-") })
+
+  pokemonImage = viewChild<ElementRef>("pokemonImage")
+
+  remainingHp = computed(() => {
+    const hp = this.actualHp() - this.damageTaken()
+    return Math.max(hp, 0)
+  })
+
+  hpPercentage = computed(() => {
+    const previouslyDamage = this.hpBase() - this.actualHp()
+    const totalDamage = previouslyDamage + this.damageTaken()
+
+    const percentage = 100 - (totalDamage / this.hpBase()) * 100
+    return Math.max(percentage, 0)
+  })
+
+  hpBarColor = computed(() => {
+    if (this.hpPercentage() < 20) {
+      return this.RED
+    } else if (this.hpPercentage() <= 50) {
+      return this.YELLOW
+    } else {
+      return this.GREEN
+    }
+  })
+
+  RED = "#f33d42"
+  YELLOW = "#fe9901"
+  GREEN = "#30ca2e"
+
   imageScale = 1.2
 
-  _hpBase: number
-  _actualHp: number
-  _spriteName: string
-  _actualSpriteName: string
-  _damageTaken: number
-  _item: string
-
-  @Input()
-  name: string
-
-  @Input()
-  get hpBase(): number {
-    return this._hpBase
-  }
-
-  public set hpBase(hpBase: number) {
-    this._hpBase = hpBase
-    this.updateHpValues()
-  }
-
-  @Input()
-  get actualHp(): number {
-    return this._actualHp
-  }
-
-  public set actualHp(actualHp: number) {
-    this._actualHp = actualHp
-    this.updateHpValues()
-  }
-
-  @Input()
-  get spriteName(): string {
-    return this._spriteName
-  }
-
-  set spriteName(spriteName: string) {
-    this._spriteName = spriteName
-  }
-
-  @Input()
-  reverse: boolean
-
-  @Input()
-  get item(): string {
-    return this._item
-  }
-
-  public set item(item: string) {
-    this._item = item.toLowerCase().replaceAll(" ", "-")
-  }
-
-  @Input()
-  get damageTaken(): number {
-    return this._damageTaken
-  }
-
-  public set damageTaken(damageTaken: number) {
-    this._damageTaken = damageTaken
-    this.updateHpValues()
-  }
-
-  @ViewChild("pokemonImage", { static: false })
-  pokemonImage: ElementRef<HTMLImageElement>
-
-  ngAfterViewInit() {
-    this._actualSpriteName = this.spriteName
-    this.removeTransparentSpace()
-  }
-
   onImageLoad() {
-    if (this.spriteName != this._actualSpriteName) {
-      this._actualSpriteName = this.spriteName
+    if (this.spriteName() != this._actualSpriteName) {
+      this._actualSpriteName = this.spriteName()
       this.removeTransparentSpace()
     }
   }
 
   canShowItemImage(): boolean {
-    return this.item != Items.instance.withoutItem()
-  }
-
-  private updateHpValues() {
-    this.setRemainingHp()
-    this.setHpPercentage()
-    this.setHpBarColor()
-  }
-
-  private setRemainingHp() {
-    const hp = this.actualHp - this._damageTaken
-    this.remainingHp = Math.max(hp, 0)
-  }
-
-  private setHpPercentage() {
-    const previouslyDamage = this.hpBase - this.actualHp
-    const totalDamage = previouslyDamage + this._damageTaken
-
-    const percentage = 100 - (totalDamage / this.hpBase) * 100
-    this.hpPercentage = Math.max(percentage, 0)
-  }
-
-  private setHpBarColor() {
-    if (this.hpPercentage < 20) {
-      this.hpBarColor = "#f33d42" //red
-    } else if (this.hpPercentage <= 50) {
-      this.hpBarColor = "#fe9901" //yellow
-    } else {
-      this.hpBarColor = "#30ca2e" //green
-    }
+    return this.item() != Items.instance.withoutItem()
   }
 
   removeTransparentSpace() {
-    if (this.pokemonImage) {
-      const image = new Image()
-      image.src = this.pokemonImage?.nativeElement.src
+    const image = new Image()
+    image.src = this.pokemonImage()!.nativeElement.src
 
-      image.onload = () => {
-        const canvas = document.createElement("canvas")
-        const ctx = canvas.getContext("2d", { willReadFrequently: true })
+    image.onload = () => {
+      const canvas = document.createElement("canvas")
+      const ctx = canvas.getContext("2d", { willReadFrequently: true })
 
-        if (ctx) {
-          canvas.width = image.width
-          canvas.height = image.height
+      if (ctx) {
+        canvas.width = image.width
+        canvas.height = image.height
 
-          ctx.drawImage(image, 0, 0)
+        ctx.drawImage(image, 0, 0)
 
-          const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
-          const data = imageData.data
+        const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+        const data = imageData.data
 
-          let top = canvas.height,
-            bottom = 0,
-            left = canvas.width,
-            right = 0
+        let top = canvas.height,
+          bottom = 0,
+          left = canvas.width,
+          right = 0
 
-          for (let y = 0; y < canvas.height; y++) {
-            for (let x = 0; x < canvas.width; x++) {
-              const index = (y * canvas.width + x) * 4
-              const alpha = data[index + 3]
+        for (let y = 0; y < canvas.height; y++) {
+          for (let x = 0; x < canvas.width; x++) {
+            const index = (y * canvas.width + x) * 4
+            const alpha = data[index + 3]
 
-              if (alpha > 0) {
-                if (y < top) top = y
-                if (y > bottom) bottom = y
-                if (x < left) left = x
-                if (x > right) right = x
-              }
+            if (alpha > 0) {
+              if (y < top) top = y
+              if (y > bottom) bottom = y
+              if (x < left) left = x
+              if (x > right) right = x
             }
           }
-
-          const visibleHeight = bottom - top + 1
-          const visibleWidth = right - left - +1
-
-          const croppedData = ctx.getImageData(left, top, visibleWidth, visibleHeight)
-
-          canvas.height = visibleHeight
-          canvas.width = visibleWidth
-
-          this.imageScale = this.calculateImageScale(visibleHeight)
-
-          ctx.putImageData(croppedData, 0, 0)
-
-          this.pokemonImage.nativeElement.src = canvas.toDataURL()
         }
+
+        const visibleHeight = bottom - top + 1
+        const visibleWidth = right - left - +1
+
+        const croppedData = ctx.getImageData(left, top, visibleWidth, visibleHeight)
+
+        canvas.height = visibleHeight
+        canvas.width = visibleWidth
+
+        this.imageScale = this.calculateImageScale(visibleHeight)
+
+        ctx.putImageData(croppedData, 0, 0)
+
+        this.pokemonImage()!.nativeElement.src = canvas.toDataURL()
       }
     }
   }
