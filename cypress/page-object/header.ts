bff5bc4311de315b493ad5af3ecb1163ebf9dd8c
Robson Bittencourt <robson.luizv@gmail.com>
2026-05-27 13:05:22 -0300

    chore: Use SSR version to run e2e tests

--- cypress/page-object/header.ts
+++ cypress/page-object/header.ts
@@ -1,9 +1,11 @@
 export class Header {
   selectChampions() {
-    cy.contains("mat-button-toggle", "Champions").find("button").click({ force: true }).wait(200)
+    cy.contains("mat-button-toggle", "Champions").find("button").click({ force: true })
+    cy.contains("mat-button-toggle", "Champions").should("have.class", "mat-button-toggle-checked")
   }
 
   selectSv() {
-    cy.contains("mat-button-toggle", "SV").find("button").click({ force: true }).wait(200)
+    cy.contains("mat-button-toggle", "SV").find("button").click({ force: true })
+    cy.contains("mat-button-toggle", "SV").should("have.class", "mat-button-toggle-checked")
   }
 }
