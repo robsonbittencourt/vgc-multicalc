cff95c87981fd9190cb8125ad09972600ade414b
Robson Bittencourt <robson.luizv@gmail.com>
2026-08-20 22:22:48 -0300

    chore: Speed up test suite by disabling coverage by default

---
+++ vitest.domain.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vitest/config"
+
+export default defineConfig({
+  test: {
+    testTimeout: 30000,
+    environment: "node",
+    coverage: {
+      provider: "istanbul"
+    }
+  }
+})
